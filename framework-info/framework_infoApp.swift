792f59b532d614439c528e564aef0a5649cce743
eshtokin <eshtoki@gmail.com>
2022-06-06 23:40:32 +0300

    Initial Commit

---
+++ framework-info/framework_infoApp.swift
@@ -0,0 +1,17 @@
+//
+//  framework_infoApp.swift
+//  framework-info
+//
+//  Created by Alex Eshtokin on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct framework_infoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
